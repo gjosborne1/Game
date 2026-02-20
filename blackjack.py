488c4797903635478781b29d536d0ed85d6d9ccb
gjosborne1 <gjosborne81@1770.us>
2026-02-20 10:01:16 -0500

    blackjack rules

--- blackjack.py
+++ blackjack.py
@@ -68,11 +68,46 @@ Type "quit" to quit
         load(1)
         match inp:
             case "play":
-                print() #DO THIS
+                bet=100
+                if balance>=.01:
+                    bet_entered=False
+                    while not bet_entered:
+                        try:
+                            inp = round(float(input("Enter bet amount:\n")), 2)
+                        except ValueError:
+                            print("Invalid input, please type a number")
+                        else:
+                            if inp>balance:
+                                print(f"Bet cannot exceed current money amount\nCurrent balance: ${balance:.2f}")
+                            elif inp<.01:
+                                print("Bet cannot be negative or less than 1 cent")
+                            else:
+                                bet=inp
+                                bet_entered=True
+                    clear()
+                else:
+                    print("No money to bet. Bet automatically set to $100")
+                #PUT CODE FOR GAME HERE  <---
             case "balance":
                 print(f"Current balance: ${balance:.2f}")
             case "help":
-                print() #Explanation goes here
+                print("""Blackjack rules:
+---
+The goal of blackjack is to have your hand as close to 21 as possible without going over
+
+Number cards are worth face value, face cards are all worth 10, and aces are worth either 11 or 1, depending on whether or not 11 would cause your current hand to exceed 21
+
+If you go over 21, you bust and are immediately out of the game
+
+
+The player and the dealer both start with 2 cards. One of the dealer's cards is face up, and the other is initially hidden
+After looking at your cards, you can either hit (draw another card), or stand (end your turn). You can hit as many times as you want unless you go over 21
+At the start of your turn, you can also choose to double down, which doubles your bet and lets you draw one more card before immediately ending your turn
+
+The dealer follows specific rules regarding when he must hit or stand. If the dealer busts or has a lower hand than you, you win
+
+Payout for a win is 1 to 1. If you start the game with a blackjack (a face card and an ace), there is a 3 to 2 payout instead
+---""")
             case "delete save" | "delete data":
                 if file_perm:
                     delete("Are you sure you want to delete your saved data", True)
